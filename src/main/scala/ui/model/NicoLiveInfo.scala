8b3d393b6f0b1ea0b0d317e925dfad1854955864
kodama <manche.451925@gmail.com>
2015-06-24 02:04:50 +0900

    mist36 NicoLiveRankingを修正。NicoLiveInfo実装

--- src/main/scala/ui/model/NicoLiveInfo.scala
+++ src/main/scala/ui/model/NicoLiveInfo.scala
@@ -3,7 +3,30 @@ package ui.model
 /**
  * Created by mist on 2015/06/10.
  */
-class NicoLiveInfo {
+class NicoLiveInfo(builder: NicoLiveInfoBuilder) {
+  val id: Int = builder.id toInt
+  val hideZapping: Boolean = builder.hide_zapping
+  val isNsen: Boolean = builder.is_nsen
+  val isProduct: Boolean = builder.is_product
+  val isZappingModeEnabled: Boolean = builder.is_zapping_mode_enabled
+  val thumbnailSmallUrl: String = builder.thumbnail_small_url
+  val title: String = builder.title
+//   val viewCounter: Int = builder.view_counter toInt
 
+  // Add and multiply by prime numbers
+  override def hashCode = id
+
+  override def equals(other:Any) = other match {
+    case that: NicoLiveInfo => (that canEqual other) && (this.id == that.id)
+    case _ => false
+  }
+
+  private def canEqual(other: Any) = {
+    other.isInstanceOf[NicoLiveInfo]
+  }
 
 }
+
+case class OnAirStreamInfo(onair_stream_list: List[NicoLiveInfoBuilder])
+case class NicoLiveInfoBuilder(hide_zapping: Boolean,id: String, is_nsen: Boolean, is_product: Boolean, is_zapping_mode_enabled: Boolean, thumbnail_small_url: String, title: String, view_counter: String)
+
--- src/main/scala/ui/model/NicoLiveRanking.scala
+++ src/main/scala/ui/model/NicoLiveRanking.scala
@@ -4,22 +4,46 @@ import dispatch._
 import org.json4s._
 import org.json4s.native.JsonMethods._
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.collection.mutable.Set
 
 /**
  * Created by mist on 2015/06/10.
  */
 class NicoLiveRanking {
 
-  val rankUrl = "http://live.nicovideo.jp/api/getzappinglist?zroute=recent&zpage=0&sort=comment_num&order=desc"
+  // http://www59.atwiki.jp/nicoapi/pages/33.html
 
-  val rankReq = url(rankUrl).GET.addCookie(LoginInfo.getUserSession)
-  val rankRes = Http(rankReq)
+  // 正体不明
+  implicit val formats = DefaultFormats
 
-  println(rankRes().getResponseBody)
 
-  val json = rankRes().getResponseBody
+  val nicoLiveInfoSet =  Set.empty[NicoLiveInfo]
+  for (index <- 0 until 10) {
+    val rankingUrl = s"http://live.nicovideo.jp/api/getzappinglist?zroute=recent&zpage=${index}&sort=comment_num&order=desc"
+    val rankReq = url(rankingUrl).GET.addCookie(LoginInfo.getUserSession)
+    val rankRes = Http(rankReq)
 
-  println(parse(json))
-  println(parse(json).values)
+    val json = rankRes().getResponseBody
+    val onAirStreamInfo = parse(json).extract[OnAirStreamInfo]
+
+    println(onAirStreamInfo.onair_stream_list.size)
+
+    onAirStreamInfo.onair_stream_list foreach( f =>  nicoLiveInfoSet += new NicoLiveInfo(f))
+  }
+  for (index <- 0 until 10) {
+    val rankingUrl = s"http://live.nicovideo.jp/api/getzappinglist?zroute=recent&zpage=${index}&sort=view_counter&order=desc"
+    val rankReq = url(rankingUrl).GET.addCookie(LoginInfo.getUserSession)
+    val rankRes = Http(rankReq)
+
+    val json = rankRes().getResponseBody
+    val onAirStreamInfo = parse(json).extract[OnAirStreamInfo]
+
+    println(onAirStreamInfo.onair_stream_list.size)
+
+    onAirStreamInfo.onair_stream_list foreach( f =>  nicoLiveInfoSet += new NicoLiveInfo(f))
+  }
+  println("nicoLiveInfoSet.size" + nicoLiveInfoSet.size)
+
 
 }
+
