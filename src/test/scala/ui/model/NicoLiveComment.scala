a073d4014ab95e23480223bc23d5ed6739365182
TestUser <you@example.com>
2015-10-07 21:12:41 +0900

    ニコ生コメント取得しようとしてる最中

--- src/test/scala/ui/model/NicoLiveComment.scala
+++ src/test/scala/ui/model/NicoLiveComment.scala
@@ -2,6 +2,7 @@ package ui.model
 
 import dispatch._
 import util.NicoMovieUtils
+import scala.xml.XML
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -15,18 +16,23 @@ class NicoLiveComment(inputText: String) {
 //  private def initComments:List[NicoCommentModel] = {
   private def initComments = {
 
-    val no = NicoMovieUtils.extractMovieNo(inputText)
+//    val no = NicoMovieUtils.extractMovieNo(inputText)
+    val no = "lv237725731"
 
     // val getPlayerStatus = s"http://watch.live.nicovideo.jp/api/getplayerstatus?v=$no"
     // val getPlayerStatus = s"http://live.nicovideo.jp/api/getplayerstatus?v=$no"
-    val getPlayerStatus = s"http://live.nicovideo.jp/api/getplayerstatus/$no"
-
-    println(getPlayerStatus)
+    val getPlayerStatus = s"http://live.nicovideo.jp/api/getplayerstatus?v=$no"
 
     val reqPlayerStatus = url(getPlayerStatus).POST.addCookie(LoginInfo.getUserSessionByCookie)
 
     val reqFuturePlayerStatus = Http(reqPlayerStatus)
     println(reqFuturePlayerStatus().getResponseBody)
+
+    val decoded = new String(reqFuturePlayerStatus().getResponseBodyAsBytes, "UTF-8")
+    val xml = XML.loadString(decoded)
+    val port = xml \\ "addr"
+    println(port)
+
 
   }
 
--- src/test/scala/ui/model/NicoLiveCommentSpec.scala
+++ src/test/scala/ui/model/NicoLiveCommentSpec.scala
@@ -9,7 +9,7 @@ class NicoLiveCommentSpec extends Specification {
   "NicoMovieComment" should {
     "case: test" in {
       // 動作確認のためのテスト
-      val comment = new NicoLiveComment("234765468")
+      val comment = new NicoLiveComment("237725731")
       //comment.comments.foreach(m => println(m.comment))
       1 must equalTo(1)
     }
